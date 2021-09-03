15bf4faa4e82db395083d86c4c0e8667a805c261
Nishedha Sri Lak Hemacandra <nishedha.srilak@gmail.com>
2021-09-03 09:51:36 +0530

    On branch master

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title}) => {
-    const onClick = ()=>{
-        console.log('clicked');
-    }
+const Header = ({title,onAdd,showAddTask}) => {
     return(
     <header className='header'>
         <h1>{title} </h1>
-        <Button color='green'text='Add' onClick={onClick}/>
+        <Button 
+            color={showAddTask ? 'red': 'green' }
+            text={showAddTask ? 'Close' : 'Add'}
+            onClick={onAdd}/>
     </header>
 
     )
